024661b1d51db52c020fa3166d0938a7059e6f66
PravinS14 <pravin14@outlook.com>
2026-07-04 19:07:14 +0530

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string enc;
+        for(auto &str : strs){
+            enc += str+".";
+        }
+        return enc;
+    }
+
+    vector<string> decode(string s) {
+        vector<string> dec;
+        string temp;
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] != '.'){
+                temp += s[i];
+            }
+            else{
+                dec.push_back(temp);
+                temp.erase();
+            }
+        }
+    return dec;    
+    }
+};
