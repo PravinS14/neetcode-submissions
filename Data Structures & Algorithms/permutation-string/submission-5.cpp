9b29e9dc63e1c7692b970d0bf5133b889335ffb7
PravinS14 <pravin14@outlook.com>
2026-07-14 20:58:04 +0530

    Add: permutation-string - submission-5

---
+++ Data Structures & Algorithms/permutation-string/submission-5.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        int left =0;
+        //int right =0;
+        int end = left + s1.size()-1;
+        sort(s1.begin(),s1.end());
+        while(end < s2.size()){
+            string temp;
+            //int count =0;
+            if(left <= (s2.size()-s1.size())){
+                temp = s2.substr(left,s1.size());
+                sort(temp.begin(),temp.end());
+                if(temp == s1){
+                    return true;
+                }else{
+                    left++;
+                    end = left + s1.size()-1;
+                }
+            }
+            else{
+                break;
+            }
+        }
+        return false;
+    }
+};
